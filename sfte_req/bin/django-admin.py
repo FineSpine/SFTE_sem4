15b6967605e62cb5a44d272b1194e227cea47b7a
Khush <khushkanjani26@gmail.com>
2022-06-30 13:49:39 +0530

    Initial commit

---
+++ sfte_req/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/nokturnal/Documents/Group1_InternationalConferenceManagement/source_code/sfte_req/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
